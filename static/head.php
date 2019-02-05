d6b845ab60c769c81ee0ba7aa5d8f26e62c6b5f5
Subilan <1843917482@qq.com>
2019-02-05 14:45:16 +0800

    📕 内容更新

--- static/head.php
+++ static/head.php
@@ -1,2 +1,8 @@
 <head>
-    <link rel="stylesheet" href="/"
+    <link rel="stylesheet" href="/css/mdui.min.css">
+    <script src="/js/mdui.min.js"></script>
+    <script src="/js/ajax.js"></script>
+    <script src="/js/global.js"></script>
+    <script src="/js/js.cookie-2.2.0.min.js"></script>
+    <script src="/js/init.js"></script>
+</head>

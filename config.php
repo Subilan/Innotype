1ab67e59e74957855d6830d5eb5c715d8f983f8f
Subilan <suyuanliang1995@gmail.com>
2019-11-23 17:58:01 +0800

    配置文件设定

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * config.php
+ * 自定义设置项目
+ */
+
+return [
+    // 网站名称
+    "name" => "Innotype",
+    // 网站简介
+    "desc" => "A simple blog.",
+    // 个人头像
+    "avatar" => "https://i.loli.net/2019/11/23/wJRt3L5VW9NugAC.jpg",
+    // 个人简介
+    "personal-desc" => "Just a programmer.",
+    // 主题主色
+    "primary-color" => "indigo",
+    // 主题强调色，可以为 null
+    "accent-color" => null,
+    // 自定义页面左侧菜单，如果不熟悉可以前往后台图形化界面配置
+    "custom-list" => [
+        /**
+         * @example
+        [
+            "icon" => "mdi-xxx",
+            "text" => "按钮文字",
+            "action" => "console.log('hello, world!')",
+        ]
+        */
+    ]
+];
